5fdc3e532f01ba94e483f6b491760142ee32c9cc
Daryl McCullough <stevendaryl3016@yahoo.com>
2017-07-04 21:56:41 +0000

    do some styling

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,18 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap-sprockets
 //= require turbolinks
+
+$(document).on('turbolinks:load', function() {
+
+  $('.carousel').carousel({
+             interval: 3000
+         })
+
+});
+
+
 //= require_tree .
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
+  get 'welcome/about'
+
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations'
   }
+  
+  root 'welcome#index'
+
 end
