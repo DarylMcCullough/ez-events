1247ed54c465f3147909e80e0aaf2b7e7a92b93e
Daryl McCullough <stevendaryl3016@yahoo.com>
2017-07-07 23:03:35 +0000

    set config.action_mailer.default_url_options for production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,7 +88,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  
+  config.action_mailer.default_url_options = { host: 'https://ez-events.herokuapp.com/' }
   config.serve_static_assets = true
   config.assets.compile = true
 end
